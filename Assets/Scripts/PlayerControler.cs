5be4562566be7aa221f3c215b4d03c3eb4e72afc
Donray Williams <w.donray@yahoo.com>
2017-04-21 23:44:16 -0500

    Finished

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Have Won";
+        }
+    }
+}
