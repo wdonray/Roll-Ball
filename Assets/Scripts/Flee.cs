6bfa6615f1eb66595d817afba65d1e8fc8b82466
Donray Williams <w.donray@yahoo.com>
2017-04-25 19:41:04 -0500

    Added Flee Script

---
+++ Assets/Scripts/Flee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flee : MonoBehaviour {
+
+    public GameObject target;
+    public int max_velocity;
+
+    private Vector3 velocity;
+    private Vector3 force;
+    private Vector3 displacement;
+    private Vector3 scale_displacement;
+    private Vector3 new_force;
+
+    // Use this for initialization
+    void Start () {
+        velocity = new Vector3(0, 0, 0);
+        force = new Vector3(0, 0, 0);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        displacement = target.transform.position - transform.position;
+        scale_displacement = displacement.normalized * max_velocity * -1;
+        new_force = scale_displacement - velocity;
+        force = new_force;
+        velocity += (force * Time.deltaTime);
+        if (velocity.magnitude > max_velocity)
+            velocity = velocity.normalized * max_velocity;
+        transform.position += (velocity * Time.deltaTime);
+    }
+}
